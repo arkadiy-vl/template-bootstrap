d3e1b188482f5763e0b704f5726e838a74925483
Arkadiy_vl <arkadiy_vl@inbox.ru>
2016-09-30 20:58:27 +0500

    Начальный коммит

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+// jquery
+//no jquery/jquery.js
+
+
+// bootstrap
+//= bootstrap/affix.js
+//= bootstrap/transition.js
+//= bootstrap/tooltip.js
+//= bootstrap/alert.js
+//= bootstrap/button.js
+//= bootstrap/carousel.js
+//= bootstrap/collapse.js
+//= bootstrap/dropdown.js
+//= bootstrap/modal.js
+//= bootstrap/popover.js
+//= bootstrap/scrollspy.js
+//= bootstrap/tab.js
+
+
+// custom
+//= custom/custom.js
